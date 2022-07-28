a6b9eb5da9d30ea571b8dd799d3db61d28087ff9
Viktor Babrian <viktor.babrian@gmail.com>
2022-07-28 10:17:28 +0200

    bootloader: add possibility for after_init_hook to specify boot partition
    Signed-off-by: Viktor Babrian <viktor.babrian@gmail.com>

--- components/bootloader/subproject/main/bootloader_hooks.h
+++ components/bootloader/subproject/main/bootloader_hooks.h
@@ -33,6 +33,6 @@ void __attribute__((weak)) bootloader_before_init(void);
  * @brief Function executed *after* the second stage bootloader initialization,
  * if provided.
  */
-void __attribute__((weak)) bootloader_after_init(void);
+int __attribute__((weak)) bootloader_after_init(void);
 
 #endif // BOOTLOADER_HOOKS_H
